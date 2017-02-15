9252322aef6a0dd187c1712b816e014896b4c755
Joas Schilling <coding@schilljs.com>
2017-02-15 12:33:36 +0100

    Add a background job for all users on login
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ lib/AppInfo/Application.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017 Joas Schilling <coding@schilljs.com>
+ *
+ * @author Joas Schilling <coding@schilljs.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\QuotaWarning\AppInfo;
+
+use OCA\QuotaWarning\Job\User;
+use OCP\AppFramework\App;
+use OCP\Util;
+
+class Application extends App {
+	const APP_ID = 'quota_warning';
+
+	public function __construct() {
+		parent::__construct(self::APP_ID);
+	}
+
+	public function register() {
+		$this->registerLoginHook();
+	}
+
+
+	protected function registerLoginHook() {
+		Util::connectHook('OC_User', 'post_login', $this, 'login');
+	}
+
+	/**
+	 * @param array $params
+	 */
+	public function loginHook(array $params) {
+		if (!isset($params['uid'])) {
+			return;
+		}
+
+		$jobList = $this->getContainer()->getServer()->getJobList();
+		$jobList->add(
+			User::class,
+			['uid' => $params['uid']]
+		);
+	}
+}
---
+++ lib/Migration/Install.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017 Joas Schilling <coding@schilljs.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\QuotaWarning\Migration;
+
+use OCA\QuotaWarning\Job\User;
+use OCP\BackgroundJob\IJobList;
+use OCP\IUser;
+use OCP\IUserManager;
+use OCP\Migration\IOutput;
+use OCP\Migration\IRepairStep;
+
+class Install implements IRepairStep {
+
+	/** @var IUserManager */
+	protected $userManager;
+
+	/** @var IJobList */
+	protected $jobList;
+
+	/**
+	 * Install constructor.
+	 *
+	 * @param IUserManager $userManager
+	 * @param IJobList $jobList
+	 */
+	public function __construct(IUserManager $userManager, IJobList $jobList) {
+		$this->userManager = $userManager;
+		$this->jobList = $jobList;
+	}
+
+	/**
+	 * Returns the step's name
+	 *
+	 * @return string
+	 * @since 9.1.0
+	 */
+	public function getName() {
+		return 'Add background jobs for existing users';
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @since 9.1.0
+	 */
+	public function run(IOutput $output) {
+		$output->startProgress();
+		$this->userManager->callForSeenUsers(function(IUser $user) use($output) {
+			$this->jobList->add(
+				User::class,
+				['uid' => $user->getUID()]
+			);
+			$output->advance();
+		});
+		$output->finishProgress();
+	}
+}
