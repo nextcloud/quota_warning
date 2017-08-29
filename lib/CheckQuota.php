b595a53120e1610108b7b7c3b383c19e91d8c8ba
Joas Schilling <coding@schilljs.com>
2017-08-29 13:56:51 +0200

    Make compatible with current 12.0.2
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/CheckQuota.php
+++ lib/CheckQuota.php
@@ -176,9 +176,12 @@ class CheckQuota {
 		$lang = $this->config->getUserValue($userId, 'core', 'lang');
 		$l = $this->l10nFactory->get('quota_warning', $lang);
 		$emailTemplate = $this->mailer->createEMailTemplate();
-		$emailTemplate->setMetaData('quota_warning.Notification', [
-			'quota' => $percentage,
-		]);
+
+		if (method_exists($emailTemplate, 'setMetaData')) {
+			$emailTemplate->setMetaData('quota_warning.Notification', [
+				'quota' => $percentage,
+			]);
+		}
 
 		$emailTemplate->addHeader();
 		$emailTemplate->addHeading($l->t('Reaching quota limit'), false);
